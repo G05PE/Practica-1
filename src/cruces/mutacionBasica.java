d499e7cff719ee8545af8bfc60e9b892c59bc3df
Sergio <31887668+sramos02@users.noreply.github.com>
2020-02-19 23:25:40 +0100

    Update mutacionBasica.java

--- src/cruces/mutacionBasica.java
+++ src/cruces/mutacionBasica.java
@@ -1,5 +1,22 @@
 package cruces;
 
-public class mutacionBasica {
+public class mutacionBasica extends mutacion{
 
+  //Constructor
+ public mutacionBasica(double probMutcion, individuo recibido){
+    super(probMutcion, individuo);
+ }
+  
+  @Override
+  public individuo mutar(individuo recibido){
+    individuo mutado = recibido;
+    
+    for(int i = 0; i < log2(recibido); i++){
+      double rand = Math.random()%1;
+      if(rand < PMutacion){ //Que narices es PMutacion en los apuntes del tema 3?
+        mutado.getIndividuo[i].getCromosoma() = !recibido.individuo[i].cromosoma();
+      }
+    }  
+    
+    return mutado;
 }
