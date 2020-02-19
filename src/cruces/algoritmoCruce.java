2d1c85ff4682b20282f0f1a8f7ab1056018eeb91
Sergio <31887668+sramos02@users.noreply.github.com>
2020-02-19 23:27:39 +0100

    Update algoritmoCruce.java

--- src/cruces/algoritmoCruce.java
+++ src/cruces/algoritmoCruce.java
@@ -3,23 +3,32 @@ package cruces;
 import poblacion.poblacion;
 
 public abstract class algoritmoCruce {
+	private double probCruce;
+	private poblacion seleccionados;
+	private poblacion reproductores;
+	private poblacion descendientes;
 	
-	private static algoritmoCruce cruce = null;
-	public abstract poblacion iniciarPoblacion(poblacion pb, float porcnt);
-	
-	public static void createInstance(String cruces) {
-		switch (cruces) {
-		case "MONOPUNTO":
-			cruce = new monopunto();
-			break;
-		case "UNIFORME":
-			cruce = new uniforme();
-		default:
-			break;
-		}
+	public algoritmoCruce(double probCruce, poblacion seleccionados) {
+		this.setProbCruce(probCruce);
+		this.seleccionados=seleccionados;
+		reproductores=new poblacion(seleccionados.getSize(), seleccionados.getPrecision(), 
+				seleccionados.getFuncion());
+		descendientes=new poblacion(seleccionados.getSize(), seleccionados.getPrecision(), 
+				seleccionados.getFuncion());
 	}
 	
-	public static algoritmoCruce getInstance() {
-		return algoritmoCruce.cruce;
+	public abstract poblacion cruzar();
+	
+	public double getProbCruce() {
+		return probCruce;
+	}
+	public void setProbCruce(double probCruce) {
+		this.probCruce = probCruce;
+	}
+	public poblacion getSeleccionados() {
+		return seleccionados;
+	}
+	public poblacion getDescendientes() {
+		return descendientes;
 	}
 }
