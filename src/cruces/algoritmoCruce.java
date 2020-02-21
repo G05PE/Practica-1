c7cc1905fcd8bc4ce098ca405b317d792dcad9fa
Sergio <31887668+sramos02@users.noreply.github.com>
2020-02-21 14:10:10 +0100

    Update algoritmoCruce.java

--- src/cruces/algoritmoCruce.java
+++ src/cruces/algoritmoCruce.java
@@ -17,25 +17,7 @@ public abstract class algoritmoCruce {
 				seleccionados.getFuncion());
 	}
 	public abstract poblacion cruzar();
-	
-	/*private static algoritmoCruce cruce = null;
-	public abstract poblacion iniciarPoblacion(poblacion pb, float porcnt);
-	
-	public static void createInstance(String cruces) {
-		switch (cruces) {
-		case "MONOPUNTO":
-			cruce = new monopunto();
-			break;
-		case "UNIFORME":
-			cruce = new uniforme();
-		default:
-			break;
-		}
-	}
-	
-	public static algoritmoCruce getInstance() {
-		return algoritmoCruce.cruce;
-	}*/
+
 	public double getProbCruce() {
 		return probCruce;
 	}
