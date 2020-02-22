59be80f9d188cebd6b74ef32befc6ed1e9b00a09
Sergio <sramos02@ucm.es>
2020-02-22 23:08:18 +0100

    Empeznado las modificaciones

--- src/cruces/uniforme.java
+++ src/cruces/uniforme.java
@@ -1,5 +1,8 @@
 package cruces;
 
+import java.util.List;
+
+import genetica.gen;
 import poblacion.individuo;
 import poblacion.poblacion;
 
@@ -8,13 +11,30 @@ public class uniforme extends algoritmoCruce{ //Tema 3 pagina 12
 	
 	public uniforme(double probCruce, poblacion seleccionados) {
 		super(probCruce, seleccionados);
-		// TODO Auto-generated constructor stub
 	}
 
 	
+	//Por cada alelo se pregunta por la probabilidad de cruce, si la supera se cambia
+	//sino se queda igual
+	
 	@Override
 	public poblacion cruzar() {
-		seleccionaReproductores();
+		for(int i = 0; i < getSeleccionados().getSize(); i++) {
+			individuo indActual = getSeleccionadoConcreto(i);
+			addDescendiente(indActual);
+			
+			for(int j = 0; j < indActual.getCromosoma().size(); j++) {
+				gen genAct = indActual.getCromosomaAt(i);			
+				for(int k = 0; k < genAct.getGenotipo().size(); k++) {
+					
+					boolean alelo = genAct.getGenotipo().get(k);
+					
+					if(Math.random() < getProbCruce()) {
+						getDescendiente
+					}
+				}
+			}
+		}
 		creaDescendientes();
 		return descendientes;
 	}
