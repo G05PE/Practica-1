7f2928ceb2dfd49320adce38a67b9b5d26ab1917
Sergio <sramos02@ucm.es>
2020-02-28 10:58:20 +0100

    Create discretoUniforme.java

---
+++ src/cruces/discretoUniforme.java
@@ -0,0 +1,29 @@
+package cruces;
+
+import genetica.gen;
+import poblacion.poblacion;
+
+public class discretoUniforme extends algoritmoCruce{ 
+	
+	//Por cada alelo del padre se pregunta por la probabilidad de cruce, si la supera se cambia	
+	@Override
+	public poblacion cruzar(poblacion seleccionados, double prob) {
+		for(int i = 0; i < getSeleccionados().getSize() - 1; i += 2) {
+			addDescendiente(getSeleccionadoConcreto(i));
+			addDescendiente(getSeleccionadoConcreto(i + 1));
+					
+			for(int j = 0; j < getDescendienteAt(i).getLongitud(); j++) {
+				if(Math.random()%1 < getProbCruce()) {
+					gen aux = getDescendienteAt(i).getCromosomaAt(j);
+					getDescendienteAt(i).setGen(j, getDescendienteAt(i+1).getCromosomaAt(j));
+					getDescendienteAt(i+1).setGen(j, aux);
+				}
+			}
+		}
+		
+		return getDescendientes();	
+	}
+	
+	
+	
+}
